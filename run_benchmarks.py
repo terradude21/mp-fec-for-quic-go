c86228ddeeb3c4b38df61df275590ea0dcbf60e7
etwasanderes2 <etwasanderes2+github@pm.me>
2024-07-02 07:44:00 +0200

    Add preliminary Benchmarking
    main.go is currently commented out, because of naming conflics.
    server does not shut down gracefully, but does write qlog file.
    qlogs are currently written into the directory specified by the QLOGDIR
    evirnoment variable.
    TODO:
    - give qlogs proper names
    - kill server once client is done

---
+++ run_benchmarks.py
@@ -0,0 +1,133 @@
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.log import setLogLevel, info
+from mininet.util import pmonitor
+from subprocess import STDOUT
+
+from dataclasses import dataclass
+from typing import *
+import tomllib
+from operator import mul
+from functools import reduce
+
+DEFAULT_COMMON_ARGS = {'executable': './benchmark', 'insecure': True, 'q': True, 'v': True, 'fec': [True, False], 'trace': True}
+DEFAULT_SERVER_ARGS = {'s': True}
+DEFAULT_CLIENT_ARGS = {}
+
+@dataclass
+class NetConfig:
+	loss: int
+	bandwidth: int
+	delay: int
+
+@dataclass
+class FinalConfig:
+	net_config: NetConfig
+	server_args: List[str]
+	client_args: List[str]
+	testcase_name: str
+
+def run_mininet(config: FinalConfig):
+	topo = Topo()
+	server_s = topo.addHost('server')
+	client_s = topo.addHost('client')
+	switch = topo.addSwitch('s0')
+	link1 = topo.addLink(client_s, switch, loss=config.net_config.loss, bandwidth=config.net_config.bandwidth, delay=config.net_config.delay)
+	link2 = topo.addLink(server_s, switch)
+	info('topology configured\n')
+	
+	net = Mininet(topo=topo, waitConnected=True)
+	net.start()
+	server = net.get(server_s)
+	client = net.get(client_s)
+	info('mininet started\n')
+
+	popens = dict()
+	last = net.hosts[ -1 ]
+	popens[ server ] = server.popen(*config.server_args, stderr=STDOUT)
+	popens[ client ] = client.popen(*config.client_args, stderr=STDOUT)
+	info('processes started\n')
+
+	for host, line in pmonitor( {client: popens[client]} ):
+		if host:
+			info( "<%s>: %s" % ( host.name, line ) )
+
+	net.stop()
+	info('mininet stopped\n')
+
+def convert_dict_to_args(data, pre=None, post=None):
+	args = []
+	if pre is not None:
+		args.append(pre)
+	for flag, val in data.items():
+		if val == False:
+			continue
+		if val == True:
+			args.append(f'-{flag}')
+		else:
+			args.append(f'-{flag} {val}')
+	if post is not None:
+		args.append(post)
+	return args
+
+def single_to_list(item):
+	if isinstance(item, list):
+		return item
+	else:
+		return [item]
+
+def second(tup):
+	_,x = tup
+	return x
+
+def config_iterations(config_raw):
+	config = {k: single_to_list(v) for k,v in config_raw.items()}
+	digits = [(k, len(k)) for k, v in config.items()]
+	maximum = reduce(mul, map(second, digits), 1)
+	for i in range(maximum):
+		res_conf = {}
+		index = i
+		for key, count in digits:
+			key_idx = index % count
+			index //= count
+			res_conf[key] = config[key][key_idx]
+		yield res_conf
+
+def generate_configs(toml_config, name):
+	common_config = dict(DEFAULT_COMMON_ARGS.copy(), **toml_config.get('common', dict()))
+	client_config = dict(DEFAULT_CLIENT_ARGS.copy(), **toml_config.get('client', dict()))
+	server_config = dict(DEFAULT_SERVER_ARGS.copy(), **toml_config.get('server', dict()))
+	net_config = toml_config.get('net', dict())
+	for common in config_iterations(common_config):
+		for client in config_iterations(client_config):
+			for server in config_iterations(server_config):
+				for net in config_iterations(net_config):
+					client_conf_instance = dict(common.copy(), **client)
+					server_conf_instance = dict(common.copy(), **server)
+
+					net_conf_instance = NetConfig(
+						loss= net.get('loss', 0),
+						bandwidth= net.get('bandwidth', None),
+						delay= net.get('delay', 0),
+					)
+
+					# convert to string form
+					url = client_conf_instance.pop('url')
+					executable = client_conf_instance.pop('executable')
+					client_args = convert_dict_to_args(client_conf_instance, executable, f'https://10.0.0.1:6121/{url}')
+					executable = server_conf_instance.pop('executable')
+					server_args = convert_dict_to_args(server_conf_instance, executable)
+					yield FinalConfig(net_conf_instance, client_args, server_args, name)
+
+
+
+
+def run_benchmarks():
+	conf = tomllib.load(open('benchmark_config.toml', 'rb'))
+	for testcase, testcase_conf in conf.items():
+		for config in generate_configs(testcase_conf, testcase):
+			run_mininet(config)
+
+setLogLevel( 'info' )
+run_benchmarks()
